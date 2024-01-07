8c33964f5dbe232b8da5fae5f17bc0530f898857
Axxyush <ayushsrivastava192004@gmail.com>
2024-01-07 02:39:45 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const base_url =
   "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD";
+const code_url =
+  "https://min-api.cryptocompare.com/data/blockchain/list?api_key=a6c5e5c95ccea64df807e6b130b1fe2fc9d7feb3b765e562e39990e2f71d8dc8";
 
 let amount = document.querySelector("#amount").textContent;
 let btn = document.querySelector("#btn");
@@ -8,22 +10,22 @@ let currency = document.querySelector("#currency");
 const updateAmount = async () => {
   let input = document.querySelector("#input").value;
   input = input.toUpperCase();
-  for (coin in coinList) {
-    coinCode = coinList[coin];
-    if (Object.keys(coinList).includes(input)) {
-      url = `https://min-api.cryptocompare.com/data/price?fsym=${coinList[input]}&tsyms=USD`;
-      let response = await fetch(url);
-      let data = await response.json();
-      let rate = data.USD;
-      document.querySelector("#amount").textContent = rate;
-      currency.style.display = "";
-      return;
-    } else {
-      document.querySelector("#amount").textContent =
-        "Coin not recognised by our API";
-      currency.style.display = "none";
-      return;
-    }
+  let response1 = await fetch(code_url);
+  let data1 = await response1.json();
+  let temp = Object.keys(data1.Data);
+  if (temp.includes(input)) {
+    url = `https://min-api.cryptocompare.com/data/price?fsym=${input}&tsyms=USD`;
+    let response2 = await fetch(url);
+    let data2 = await response2.json();
+    let rate = data2.USD;
+    document.querySelector("#amount").textContent = rate;
+    currency.style.display = "";
+    return;
+  } else {
+    document.querySelector("#amount").textContent =
+      "Coin not recognised by our API";
+    currency.style.display = "none";
+    return;
   }
 };
 
